fec584fbdab71c6f8b79cce0a89f2f3c69d18ae4
manjushreeat <manjushreeat035@gmail.com>
2023-06-13 11:44:09 +0000

    modify from sydeny

--- add.java
+++ add.java
@@ -1,9 +1,21 @@
-public class SumOfNumbers1  
+import java.util.Scanner;  
+public class SumOfNumbers2  
 {  
-public static void main(String args[])   
+public static void main(String args[])  
 {  
-int n1 = 225, n2 = 115, sum;  
-sum = n1 + n2;  
-System.out.println("The sum of numbers is: "+sum);  
+int x, y, sum;  
+Scanner sc = new Scanner(System.in);  
+System.out.print("Enter the first number: ");  
+x = sc.nextInt();  
+System.out.print("Enter the second number: ");  
+y = sc.nextInt();  
+sum = sum(x, y);  
+System.out.println("The sum of two numbers x and y is: " + sum);  
+}  
+//method that calculates the sum  
+public static int sum(int a, int b)  
+{  
+int sum = a + b;  
+return sum;  
 }  
 }  
